6a3ca1a228c57de5aa7b4b0f4f4c70a04ccdc28b
Zoull <zoull@zdindustries.com>
2022-06-27 00:11:38 -0600

    Fix Glyph package checksum and version

--- glyph-launcher/tools/chocolateyinstall.ps1
+++ glyph-launcher/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'Glyph*'
-  checksum      = '7534F5F29D99695E04AC322F9A3FFB101A598967CEEB2B5D56A1937AAAC68749'
+  checksum      = '25D2E86FF7E6CA61B9E1212AEB793C78D939881DB87C45D351D9C4A20BDBAD36'
   checksumType  = 'sha256'
   validExitCodes= @(0, 3010, 1641)
 }
