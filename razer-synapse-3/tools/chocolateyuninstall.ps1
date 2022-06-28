d5470bc753550fcc1eb0c7b516b9908759fa29fd
Zoull <zoull@zdindustries.com>
2022-06-28 11:51:24 -0600

    Add Razer Synapse 3 package to track

---
+++ razer-synapse-3/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+& "C:\Windows\Installer\Razer Central\RCUninstall.exe" "/U:C:\Windows\Installer\Razer Central\RCUninstall\uninstall.xml" "/S"
+& "C:\Windows\Installer\Razer Game Manager Service\RzGMSuninstall.exe" "/U:C:\ProgramData\Razer\GMS\RzGMSUninstall\uninstall.xml" "/S"
+& "C:\ProgramData\Razer\Synapse3\Uninstall\RazerStringTranslations\RazerStringTranslationsUninstall.exe" "/S"
+& "C:\ProgramData\Razer\Synapse3\Uninstall\RazerSynapseConfigurationData\RazerSynapseConfigurationDataUninstall.exe" "/S"
+& "C:\ProgramData\Razer\Synapse3\Uninstall\RazerSynapse\RazerSynapseUninstall.exe" "/S"
