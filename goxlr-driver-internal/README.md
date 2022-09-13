70717cc9100740d674c09476f9fc9f66d8599c9b
Zoull <zoull@zdindustries.com>
2022-09-12 20:00:11 -0600

    Add GoXLR Driver internal package for personal chocolatey feed to keep updated version

---
+++ goxlr-driver-internal/README.md
@@ -0,0 +1,13 @@
+![goxlr-driver Logo](https://raw.githubusercontent.com/Zoullx/chocolatey-packages/master/icons/goxlr-driver.png "GoXLR Driver Logo")
+
+# [GoXLR Driver](https://community.chocolatey.org/packages/goxlr-driver)
+
+Take everything you used to know about broadcasting audio and forget it. What would take dozens of pieces of hardware and software can now be achieved by one sleek and intuitive solution. Mix your audio in real time, change your voice, use industry defining hardware and software, and engage your audience like never before.
+
+# The Perfect Mix Every Time
+
+GoXLR gives you control of your audio like never before. With a 4-channel mixer, you can control the volume of all your audio sources in real time so you can keep your focus where it needs to be.
+
+# A Smooth Operator
+
+With customizable motorized faders, you'll always come back to sound the way you want it. Change the LCD scribble strips to personalize GoXLR and make it your own.
---
+++ goxlr-driver-internal/tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipFile     = 'GoXLR_Driver_v5.12.0_2021-06-28.zip'
+$zipLocation = Join-Path $toolsDir $zipFile
+
+$zipPackageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  file          = $zipLocation
+  unzipLocation = $toolsDir
+  checksum      = '10446A5D32B0B8F65852959C2E78EEB88A1D5F88AE2E414B28496C1FC3B5485C'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @zipPackageArgs
+
+$fileName = (Get-ChildItem $toolsDir -Filter *.exe | Select-Object -First 1).Name
+$fileLocation = Join-Path $toolsDir $fileName
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  file          = $fileLocation
+  softwareName  = 'TC-HELICON GoXLR USB Audio Driver*'
+  checksum      = 'C0B24A8D942BA906DF022A4F245EBAA9FAB5B256A0E5461F979222E29D729262'
+  checksumType  = 'sha256'
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
