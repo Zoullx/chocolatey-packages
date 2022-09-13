f79aeaf05d6743ed22941fc90320ff9b8ce7c503
Zoull <zoull@zdindustries.com>
2022-09-12 20:11:04 -0600

    Update GoXLR package to the only version that is publicly available

--- goxlr/tools/chocolateyinstall.ps1
+++ goxlr/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://mediadl.musictribe.com/media/PLM/sftp/incoming/hybris/import/goxlr/download/GoXLR_App_v1.4.25.190.zip'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url      = 'https://mediadl.musictribe.com/download/software/tchelicon/GoXLR/GoXLR-App-1.3.3.130.zip'
 
 $zipPackageArgs = @{
   packageName   = $env:ChocolateyPackageName
   url           = $url
   unzipLocation = $toolsDir
-  checksum      = '288874AC262F19E71360407AF39DBCC749A1EB87C7E10CEBC8F12F40448A77CA'
+  checksum      = '98ACDB9A349903D42B8E13C98F2923F5F587C4FA2AF85E0E452C2240C6187704'
   checksumType  = 'sha256'
 }
 
@@ -21,7 +21,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'GoXLR App*'
-  checksum      = 'F398E982E97F44188B9245A7FF2BBB1E3FD29A44100F467B82B1D0A249DA6F19'
+  checksum      = '17FC0C0186DC7595899CC94DC52B646F18C428E007CEDBD3F055449FFF156ADE'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
   validExitCodes= @(0, 3010, 1641)
