4faab562866add8f7dfa6ef18f62976dcdd4e2eb
Zoull <zoull@zdindustries.com>
2022-09-23 07:25:28 -0600

    Update Glyph Launcher package with new installer version

--- glyph-launcher/tools/chocolateyinstall.ps1
+++ glyph-launcher/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'Glyph*'
-  checksum      = '1680127F2A34A5CBE78F2941F83A2FF5BACC8D4210886D3DE15842E9C0BB5139'
+  checksum      = '49971CC09C7B52218943A20D31F7C8588B1A6BA81A0A4A98C05F7C4E3E9B7DDD'
   checksumType  = 'sha256'
   validExitCodes= @(0, 3010, 1641)
 }
