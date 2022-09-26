d022e23fce4e424481ad489325098dba6be50bf8
Zoull <zoull@zdindustries.com>
2022-09-26 11:33:04 -0600

    Start working on auto update scripts for Razer Synapse 2

--- razer-synapse-2/tools/chocolateyinstall.ps1
+++ razer-synapse-2/tools/chocolateyinstall.ps1
@@ -1,12 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://dl.razerzone.com/drivers/Synapse2/win/Web_Razer_Synapse_Installer_v2.21.24.41.exe'
+
+# DO NOT CHANGE THESE MANUALLY, USE update.ps1
+$url      = 'https://dl.razerzone.com/drivers/Synapse2/win/Web_Razer_Synapse_Installer_v2.21.24.41.exe'
+$checksum = 'CC8AFA3FB773DC68840E7D4B3423AE47E19A12BFCEEADC332CDDEC9D1D545627'
 
 $zipPackageArgs = @{
   packageName   = $env:ChocolateyPackageName
   url           = $url
   unzipLocation = $toolsDir
-  checksum      = 'CC8AFA3FB773DC68840E7D4B3423AE47E19A12BFCEEADC332CDDEC9D1D545627'
+  checksum      = $checksum
   checksumType  = 'sha256'
 }
 
@@ -16,13 +19,15 @@ $pluginsDir = Join-Path $toolsDir '$PLUGINSDIR'
 $fileName = (Get-ChildItem $pluginsDir -Filter *.exe | Select-Object -First 1).Name
 $fileLocation = Join-Path $pluginsDir $fileName
 
+$internalChecksum = 'D901DA91EDB94180BB2B814D1EEB3D9DD20E2E43652C4FDE93CCAB6CDA0A332D'
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Razer Synapse*'
-  checksum      = 'D901DA91EDB94180BB2B814D1EEB3D9DD20E2E43652C4FDE93CCAB6CDA0A332D'
+  checksum      = $internalChecksum
   checksumType  = 'sha256'
   silentArgs    = '/s /v"/qn"'
   validExitCodes= @(0, 3010, 1641)
