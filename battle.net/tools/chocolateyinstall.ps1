4e574c5be7eb207ab601c1517228e12a9c91a256
Zoull <zoull@zdindustries.com>
2022-09-26 11:12:21 -0600

    Update Battle.net version and install script to download installer and automatically grab checksum so embedded installer is no longer required

--- battle.net/tools/chocolateyinstall.ps1
+++ battle.net/tools/chocolateyinstall.ps1
@@ -1,9 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$ahkExe     = 'AutoHotKey'
-$ahkFile    = "$toolsDir\battle.net_install.ahk"
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkExe       = 'AutoHotKey'
+$ahkFile      = "$toolsDir\battle.net_install.ahk"
+$url          = 'https://us.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe&id=undefined'
 $fileLocation = Join-Path $toolsDir 'Battle.net-Setup.exe'
-# $url        = 'https://us.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe&id=undefined'
+$checksumType = 'sha256'
+
+Invoke-WebRequest $url -OutFile $fileLocation -UseBasicParsing
+$checksum = (Get-FileHash $fileLocation -Algorithm $checksumType).Hash
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,8 +15,8 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Battle.net*'
-  checksum      = 'B32A976B66110524B7CC94C056E678617045D36DE80ABB5E798C2F91E91C5DA8'
-  checksumType  = 'sha256'
+  checksum      = $checksum
+  checksumType  = $checksumType
   silentArgs    = ""
   validExitCodes= @(0, 3010, 1641)
 }
