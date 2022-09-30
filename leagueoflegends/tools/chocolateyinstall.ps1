6a3c1fd3733cd964c948b9c9f009fb87fb53db09
Zoull <zoull@zdindustries.com>
2022-09-30 17:46:50 -0600

    Add League of Legends package with auto update script

---
+++ leagueoflegends/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe'
+
+# DO NOT CHANGE THESE MANUALLY, USE update.ps1
+$checksum = '1ec37e4f424616fc2fe3a66469f52b3bb108acde9a54543d35c7a02939d4e7ce'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'League of Legends*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = "--skip-to-install"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ leagueoflegends/tools/chocolateyuninstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'League of Legends*'
+  fileType      = 'EXE'
+  silentArgs    = "--uninstall-product=league_of_legends --uninstall-patchline=live"
+  validExitCodes= @(0, 3010, 1605, 1614, 1641)
+  file          = "C:\Riot Games\Riot Client\RiotClientServices.exe"
+}
+
+Uninstall-ChocolateyPackage @packageArgs
