55ecabc56739911461f78b1ead486b2722a7b805
Zoull <zoull@zdindustries.com>
2022-10-05 09:01:59 -0600

    Comment out plugins that I don't currently care about

--- update_all.ps1
+++ update_all.ps1
@@ -36,45 +36,45 @@ $Options = [ordered]@{
     
     #NoCheckChocoVersion = $true                            #Turn on this switch for all packages
 
-    Report = @{
-        Type = 'markdown'                                   #Report type: markdown or text
-        Path = "$PSScriptRoot\Update-AUPackages.md"         #Path where to save the report
-        Params= @{                                          #Report parameters:
-            Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
-            NoAppVeyor  = $false                            #  Markdown: do not show AppVeyor build shield
-            UserMessage = "[Ignored](#ignored) | [History](#update-history) | [Force Test](https://gist.github.com/$Env:gist_id_test) | [Releases](https://github.com/$Env:github_user_repo/tags)"       #  Markdown, Text: Custom user message to show
-            NoIcons     = $false                            #  Markdown: don't show icon
-            IconSize    = 32                                #  Markdown: icon size
-            Title       = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
-        }
-    }
+    # Report = @{
+    #     Type = 'markdown'                                   #Report type: markdown or text
+    #     Path = "$PSScriptRoot\Update-AUPackages.md"         #Path where to save the report
+    #     Params= @{                                          #Report parameters:
+    #         Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
+    #         NoAppVeyor  = $false                            #  Markdown: do not show AppVeyor build shield
+    #         UserMessage = "[Ignored](#ignored) | [History](#update-history) | [Force Test](https://gist.github.com/$Env:gist_id_test) | [Releases](https://github.com/$Env:github_user_repo/tags)"       #  Markdown, Text: Custom user message to show
+    #         NoIcons     = $false                            #  Markdown: don't show icon
+    #         IconSize    = 32                                #  Markdown: icon size
+    #         Title       = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
+    #     }
+    # }
 
-    History = @{
-        Lines = 120                                         #Number of lines to show
-        Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
-        Path = "$PSScriptRoot\Update-History.md"            #Path where to save history
-    }
+    # History = @{
+    #     Lines = 120                                         #Number of lines to show
+    #     Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
+    #     Path = "$PSScriptRoot\Update-History.md"            #Path where to save history
+    # }
 
-    Gist = @{
-        Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
-        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
-        Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
-    }
+    # Gist = @{
+    #     Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
+    #     ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+    #     Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
+    # }
 
     Git = @{
         User     = ''                                       #Git username, leave empty if github api key is used
         Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
     }
 
-    GitReleases  = @{
-        ApiToken    = $Env:github_api_key                   #Your github api key
-        ReleaseType = 'package'                             #Either 1 release per date, or 1 release per package
-    }
+    # GitReleases  = @{
+    #     ApiToken    = $Env:github_api_key                   #Your github api key
+    #     ReleaseType = 'package'                             #Either 1 release per date, or 1 release per package
+    # }
 
-    RunInfo = @{
-        Exclude = 'password', 'apikey', 'apitoken'          #Option keys which contain those words will be removed
-        Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
-    }
+    # RunInfo = @{
+    #     Exclude = 'password', 'apikey', 'apitoken'          #Option keys which contain those words will be removed
+    #     Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
+    # }
 
     Mail = if ($Env:mail_user) {
             @{
