9f69127d633cf4a9dcae460d7ef209150af48f50
Zoull <zoull@zdindustries.com>
2022-10-06 13:10:29 -0600

    Update Razer Synapse 3 update script to return version so it doesn't error and knows its update status

--- razer-synapse-3/update.ps1
+++ razer-synapse-3/update.ps1
@@ -7,47 +7,32 @@ param (
 
 $ErrorActionPreference = 'Stop'
 
+$Env:au_PushUrl = 'https://chocolatey.zdl.io/chocolatey'
 $au_GalleryPackageRootUrl = 'https://chocolatey.zdl.io/chocolatey/Packages'
 $razerSynapseUrl = 'http://rzr.to/synapse-3-pc-download'
 
-function global:au_BeforeUpdate {
-  $Latest.Checksum32 = Get-RemoteChecksum -Url $Latest.Url32 -Algorithm 'SHA256'
-}
-
-function global:au_SearchReplace {
-  @{
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^[$]url\s*=\s*)'.*'"        = "`${1}'$($Latest.URL32)'"
-      "(?i)(^[$]checksum\s*=\s*)'.*'"   = "`${1}'$($Latest.Checksum32)'"
-    }
-  }
-}
-
 function global:au_GetLatest {
-  return 'ignore'
-  
-  # $response = try {
-  #   Invoke-WebRequest -Uri $razerSynapseUrl -UseBasicParsing -MaximumRedirection 0
-  # } catch {
-  #   $_.Exception.Response
-  # }
-  # if (($response.StatusCode -ne 301) -and ($response.StatusCode -ne 302)) {
-  #   throw "HTTP $($response.StatusCode) when requesting $razerSynapseUrl"
-  # }
-  # if (-not $response.Headers.Location) {
-  #   throw "No Location header returned when requesting $razerSynapseUrl"
-  # }
+  $response = try {
+    Invoke-WebRequest -Uri $razerSynapseUrl -UseBasicParsing
+  } catch {
+    $_.Exception.Response
+  }
+  if ($response.StatusCode -ne 200) {
+    throw "HTTP $($response.StatusCode) when requesting $razerSynapseUrl"
+  }
+  if (-not $response.BaseResponse.ResponseUri.AbsoluteUri) {
+    throw "No Location header returned when requesting $razerSynapseUrl"
+  }
 
-  # $downloadUrl = $response.Headers.Location
-  # if (-not ($downloadUrl -match '.*_V(?<Version>.*)\.exe')) {
-  #   throw "Could not determine version from url $downloadUrl"
-  # }
-  # $version = $matches.Version
+  $downloadUrl = $response.BaseResponse.ResponseUri.AbsoluteUri
+  if (-not ($downloadUrl -match '.*_V(?<Version>.*)\.exe')) {
+    throw "Could not determine version from url $downloadUrl"
+  }
+  $version = $matches.Version
 
-  # return @{
-  #   Version = $version
-  #   URL32   = $downloadUrl
-  # }
+  return @{
+    Version = $version
+  }
 }
 
 # run the update only if this script is not sourced by the virtual package
