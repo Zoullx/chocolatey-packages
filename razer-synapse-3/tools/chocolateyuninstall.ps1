2d8a762a1c36d3ced29a6afb7718dec7d1215e99
Zoull <zoull@zdindustries.com>
2022-11-08 17:42:17 -0700

    Cleanup uninstall script

--- razer-synapse-3/tools/chocolateyuninstall.ps1
+++ razer-synapse-3/tools/chocolateyuninstall.ps1
@@ -1,5 +1,4 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 & "C:\Windows\Installer\Razer Central\RCUninstall.exe" "/U:C:\Windows\Installer\Razer Central\RCUninstall\uninstall.xml" "/S"
 & "C:\Windows\Installer\Razer Game Manager Service\RzGMSuninstall.exe" "/U:C:\ProgramData\Razer\GMS\RzGMSUninstall\uninstall.xml" "/S"
