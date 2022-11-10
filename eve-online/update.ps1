f69964796b1efef7e863428376fa06e2e20bb953
Zoull <zoull@zdindustries.com>
2022-11-09 17:35:18 -0700

    Attempt to update auto update script to dynamically obtain download URL, doesn't work

--- eve-online/update.ps1
+++ eve-online/update.ps1
@@ -7,7 +7,7 @@ param (
 
 $ErrorActionPreference = 'Stop'
 
-$downloadUrl = 'https://www.eveonline.com/EveLauncher-2136456.exe'
+$downloadUrl = 'https://www.eveonline.com/download'
 
 function Get-RemoteFileVersion( [string] $Url ) {
   $fn = [System.IO.Path]::GetTempFileName()
@@ -17,12 +17,32 @@ function Get-RemoteFileVersion( [string] $Url ) {
   return $res
 }
 
+function global:au_BeforeUpdate {
+  $Latest.Checksum32 = Get-RemoteChecksum -Url $Latest.Url32 -Algorithm 'SHA256'
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^[$]url\s*=\s*)'.*'"        = "`${1}'$($Latest.URL32)'"
+      "(?i)(^[$]checksum\s*=\s*)'.*'"   = "`${1}'$($Latest.Checksum32)'"
+    }
+  }
+}
+
 function global:au_GetLatest {
-  $version = Get-RemoteFileVersion -Url $downloadUrl
+  Write-Host $downloadUrl
+  $download_page = Invoke-WebRequest $downloadUrl -UseBasicParsing
+  Write-Host $download_page
+  Write-Host $download_page.links
+  $url = ($download_page.links | Where-Object href -match "\.exe$" | Select-Object -First 1 -ExpandProperty href)
+  Write-Host $url
+
+  $version = Get-RemoteFileVersion -Url $url
 
   return @{
     Version = $version
-    URL32   = $downloadUrl
+    URL32   = $url
   }
 }
 
