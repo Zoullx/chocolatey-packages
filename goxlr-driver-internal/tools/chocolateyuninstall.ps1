7974f9d7dde22ba627e91db6545266e81f16f014
Zoull <zoull@zdindustries.com>
2022-11-21 09:14:18 -0700

    Fix uninstall script to get uninstall registry key

--- goxlr-driver-internal/tools/chocolateyuninstall.ps1
+++ goxlr-driver-internal/tools/chocolateyuninstall.ps1
@@ -1,11 +1,29 @@
 $ErrorActionPreference = 'Stop';
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'TC-HELICON GoXLR USB Audio Driver*'
   fileType      = 'EXE'
-  file          = "C:\Program Files\TC-HELICON\GoXLR_Audio_Driver\setup.exe"
   silentArgs    = "/U /S"
   validExitCodes= @(0, 3010, 1605, 1614, 1641)
 }
 
-Uninstall-ChocolateyPackage @packageArgs
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $file = $_.UninstallString -Split " /" | Select-Object -First 1
+
+    $packageArgs['file'] = $file
+
+    Uninstall-ChocolateyPackage @packageArgs
+    Start-Sleep -s 5
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
