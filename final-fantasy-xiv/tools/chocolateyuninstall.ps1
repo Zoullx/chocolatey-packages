0d22294bdc4213f256eab2c29c40189d0a54163d
Zoull <zoull@zdindustries.com>
2022-11-21 09:13:14 -0700

    Cleanup uninstall script

--- final-fantasy-xiv/tools/chocolateyuninstall.ps1
+++ final-fantasy-xiv/tools/chocolateyuninstall.ps1
@@ -9,7 +9,6 @@ $packageArgs = @{
   fileType      = 'EXE'
   silentArgs    = '-runfromtemp -l0x0009 -removeonly'
   validExitCodes= @(0, 3010, 1605, 1614, 1641)
-  file          = 'C:\Program Files (x86)\InstallShield Installation Information\{2B41E132-07DF-4925-A3D3-F2D1765CCDFE}\setup.exe'
 }
 
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
