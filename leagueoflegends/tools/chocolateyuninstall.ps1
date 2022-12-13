f4e36e3effe4ea858b82b28390d29124ac2ae770
Zoull <zoull@zdindustries.com>
2022-12-13 15:29:51 -0700

    Fix League of Legends install and uninstall to close all windows and uninstall everything

--- leagueoflegends/tools/chocolateyuninstall.ps1
+++ leagueoflegends/tools/chocolateyuninstall.ps1
@@ -1,6 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkExe     = 'AutoHotKey'
+$ahkFile    = "$toolsDir\leagueoflegends_uninstall.ahk"
 
-$packageArgs = @{
+$lolPackageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'League of Legends*'
   fileType      = 'EXE'
@@ -8,15 +11,44 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1605, 1614, 1641)
 }
 
-[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+[array]$key = Get-UninstallRegistryKey -SoftwareName $lolPackageArgs['softwareName']
 
 if ($key.Count -eq 1) {
   $key | % {
     $file = $_.UninstallString -Split " -" | Select-Object -First 1
 
-    $packageArgs['file'] = $file
+    $lolPackageArgs['file'] = $file
 
-    Uninstall-ChocolateyPackage @packageArgs
+    Start-Process $ahkExe $ahkFile
+    Uninstall-ChocolateyPackage @lolPackageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
+
+$rcPackageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Riot Client*'
+  fileType      = 'EXE'
+  silentArgs    = "--uninstall-product=Riot_Client --uninstall-patchline="
+  validExitCodes= @(0, 3010, 1605, 1614, 1641)
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $rcPackageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $file = $_.UninstallString -Split " -" | Select-Object -First 1
+
+    $rcPackageArgs['file'] = $file
+
+    Start-Process $ahkExe $ahkFile
+    Uninstall-ChocolateyPackage @rcPackageArgs
   }
 } elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
