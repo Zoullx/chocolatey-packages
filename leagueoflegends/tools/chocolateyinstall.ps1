33a0b88a77bd7b1604c7adbf69ae7af80ead6fc5
Zoull <zoull@zdindustries.com>
2023-01-08 12:12:35 -0700

    AU: 1 updated - leagueoflegends [skip ci]

--- leagueoflegends/tools/chocolateyinstall.ps1
+++ leagueoflegends/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe'
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$checksum = 'b064495909889b3ee1123290d857fa5d18324a57989ad39ec08bff372306636b'
+$checksum = '0a83f3aaf00130aa486dabd9ab6f14c2d948442bc758ead7247733e4385261bc'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
