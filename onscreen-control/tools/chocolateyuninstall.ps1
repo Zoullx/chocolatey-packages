cc84b21195e29b5ca456b7938d5824e262112bc3
Zoull <zoull@zdindustries.com>
2023-04-07 22:14:21 -0600

    Remove unnecessary commented code from uninstall script

--- onscreen-control/tools/chocolateyuninstall.ps1
+++ onscreen-control/tools/chocolateyuninstall.ps1
@@ -1,22 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-# $checksumType = 'sha256'
-
-# $url      = 'https://lmu.lge.com/ExternalService/onscreencontrol/win/2.0/OnScreenControl_7.95.zip'
-# $checksum = '4D5DF2C0399879B53DBC36CA958A9DCCFE6CF327A6B096EA9FC04EE90FFA3D45'
-
-# $zipPackageArgs = @{
-#   packageName   = $env:ChocolateyPackageName
-#   url           = $url
-#   unzipLocation = $toolsDir
-#   checksum      = $checksum
-#   checksumType  = $checksumType
-# }
-
-# Install-ChocolateyZipPackage @zipPackageArgs
-
-# $fileName = (Get-ChildItem $toolsDir -Filter *.exe | Select-Object -First 1).Name
-# $fileLocation = Join-Path $toolsDir $fileName
 $silentScript = Join-Path $toolsDir 'uninstall.iss'
 
 $packageArgs = @{
@@ -29,11 +12,6 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1605, 1614, 1641)
 }
 
-# Uninstall-ChocolateyPackage @packageArgs
-
-
-# "C:\Program Files (x86)\InstallShield Installation Information\{E5C1B339-0E4E-49A5-859E-5E1DE1938706}\setup.exe" -runfromtemp -l0x0409  -removeonly
-
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
 if ($key.Count -eq 1) {
@@ -44,7 +22,7 @@ if ($key.Count -eq 1) {
 
     Uninstall-ChocolateyPackage @packageArgs
     
-Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{E5C1B339-0E4E-49A5-859E-5E1DE1938706}"
+    Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{E5C1B339-0E4E-49A5-859E-5E1DE1938706}"
   }
 } elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
