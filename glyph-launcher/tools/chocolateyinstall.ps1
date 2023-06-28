1becc7bb5e0caafad569c6e0e608c15f98724610
Zoull <zoull@zdindustries.com>
2023-06-28 10:48:58 -0600

    Start process of getting Glyph Launcher to install silently

--- glyph-launcher/tools/chocolateyinstall.ps1
+++ glyph-launcher/tools/chocolateyinstall.ps1
@@ -1,35 +1,133 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-# $ahkExe   = 'AutoHotKey'
-# $ahkFile  = "$toolsDir\glyph-launcher_install.ahk"
-$url      = 'https://glyph.dyn.triongames.com/glyph/live/GlyphInstall.exe'
+$checksumType = 'sha256'
+$7zip = Join-Path "$env:ChocolateyInstall" 'tools\7z.exe'
+$glyphInstallUnzipFile = Join-Path $toolsDir 'GlyphInstall.exe'
+$glyphInstallUnzipFileDir = Join-Path $toolsDir 'GlyphInstall'
+$glyphExtractUnzipFileDir = Join-Path $glyphInstallUnzipFileDir 'Extracted'
+$glyphInstallParams = "x `"$glyphInstallUnzipFile`" -t# -aoa -bd -bb1 -o`"$glyphInstallUnzipFileDir`" -y `"*.7z`""
+$glyphextractParams = "x `"$glyphInstallUnzipFileDir\*`" -aoa -bd -bb1 -o`"$glyphExtractUnzipFileDir`" -y -r"
+$glyphInstallDir = Join-Path ${env:ProgramFiles(x86)} "Glyph"
+$glyphProgramDataConfig = Join-Path $env:ProgramData "Glyph\GlyphLibrary.cfg"
+$glyphStartMenuRunShortcutFolder = Join-Path $env:ProgramData "Microsoft\Windows\Start Menu\Programs\Glyph"
+$glyphStartMenuRunShortcut = Join-Path $env:ProgramData "Microsoft\Windows\Start Menu\Programs\Glyph\Glyph.lnk"
+$glyphStartMenuUninstallShortcut = Join-Path $env:ProgramData "Microsoft\Windows\Start Menu\Programs\Glyph\Uninstall Glyph.lnk"
+$glyphDesktopShortcut = Join-Path "$([Environment]::GetFolderPath("Desktop"))" "Glyph.lnk"
+$glyphRunTarget = Join-Path $glyphInstallDir "GlyphClient.exe"
+$glyphUninstallTarget = Join-Path $glyphInstallDir "glyphuninstall.exe"
+$url = 'https://glyph.dyn.triongames.com/glyph/live/GlyphInstall.exe'
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
 $checksum = '11c937235cdd0609c861540c11aff308b3cf6c9efbb9e4f723286e125bd919c1'
 
-$zipPackageArgs = @{
+# Download EXE
+$webFileArgs = @{
   packageName   = $env:ChocolateyPackageName
   url           = $url
-  unzipLocation = $toolsDir
+  file          = $glyphInstallUnzipFile
   checksum      = $checksum
   checksumType  = $checksumType
 }
 
-Install-ChocolateyZipPackage @zipPackageArgs
+Get-ChocolateyWebFile @webFileArgs
 
-# $packageArgs = @{
-#   packageName   = $env:ChocolateyPackageName
-#   unzipLocation = $toolsDir
-#   fileType      = 'EXE'
-#   url           = $url
-#   softwareName  = 'Glyph*'
-#   checksum      = $checksum
-#   checksumType  = 'sha256'
-#   validExitCodes= @(0, 3010, 1641)
-# }
+# Extract 7z files from EXE
+$glyphInstallExtractProcess = New-Object System.Diagnostics.Process
+$glyphInstallExtractProcess.EnableRaisingEvents = $true
 
-# Start-WaitandStop "Glyph*"
+$glyphInstallExtractProcess.StartInfo = New-Object System.Diagnostics.ProcessStartInfo($7zip, $glyphInstallParams)
+$glyphInstallExtractProcess.StartInfo.RedirectStandardOutput = $true
+$glyphInstallExtractProcess.StartInfo.RedirectStandardError = $true
+$glyphInstallExtractProcess.StartInfo.UseShellExecute = $false
+$glyphInstallExtractProcess.StartInfo.WorkingDirectory = $workingDirectory
+$glyphInstallExtractProcess.StartInfo.WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden
+$glyphInstallExtractProcess.StartInfo.CreateNoWindow = $true
 
-# Start-Process $ahkExe $ahkFile
-# Install-ChocolateyPackage @packageArgs
-# Start-CheckandStop "Glyph*"
+$glyphInstallExtractProcess.Start() | Out-Null
+if ($glyphInstallExtractProcess.StartInfo.RedirectStandardOutput) { $glyphInstallExtractProcess.BeginOutputReadLine() }
+if ($glyphInstallExtractProcess.StartInfo.RedirectStandardError) { $glyphInstallExtractProcess.BeginErrorReadLine() }
+$glyphInstallExtractProcess.WaitForExit()
+$glyphInstallExtractProcess.Dispose()
+
+# Extract Glyph files from 7z files
+$glyph7zExtractProcess = New-Object System.Diagnostics.Process
+$glyph7zExtractProcess.EnableRaisingEvents = $true
+
+$glyph7zExtractProcess.StartInfo = New-Object System.Diagnostics.ProcessStartInfo($7zip, $glyphExtractParams)
+$glyph7zExtractProcess.StartInfo.RedirectStandardOutput = $true
+$glyph7zExtractProcess.StartInfo.RedirectStandardError = $true
+$glyph7zExtractProcess.StartInfo.UseShellExecute = $false
+$glyph7zExtractProcess.StartInfo.WorkingDirectory = $workingDirectory
+$glyph7zExtractProcess.StartInfo.WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden
+$glyph7zExtractProcess.StartInfo.CreateNoWindow = $true
+
+$glyph7zExtractProcess.Start() | Out-Null
+if ($glyph7zExtractProcess.StartInfo.RedirectStandardOutput) { $glyph7zExtractProcess.BeginOutputReadLine() }
+if ($glyph7zExtractProcess.StartInfo.RedirectStandardError) { $glyph7zExtractProcess.BeginErrorReadLine() }
+$glyph7zExtractProcess.WaitForExit()
+$glyph7zExtractProcess.Dispose()
+
+# Copy Glyph files into Program Files
+Copy-Item "$glyphExtractUnzipFileDir\" $glyphInstallDir -recurse -exclude "GlyphInstall*" -Force
+
+# Create Glyph config
+New-Item $glyphProgramDataConfig -ItemType File -Value "[Glyph]" -Force
+Add-Content $glyphProgramDataConfig "Installed=true"
+
+# Create Start Menu shortcut folder
+New-Item $glyphStartMenuRunShortcutFolder -ItemType Directory -Force
+
+# Create Start Menu run shortcut
+Install-ChocolateyShortcut -ShortcutFilePath $glyphStartMenuRunShortcut -TargetPath $glyphRunTarget -WorkingDirectory "$glyphInstallDir\"
+# $glyphStartMenuRunShortcutWshShell = New-Object -comObject WScript.Shell
+# $glyphStartMenuRunShortcutProcess = $glyphStartMenuRunShortcutWshShell.CreateShortcut($glyphStartMenuRunShortcut)
+# $glyphStartMenuRunShortcutProcess.TargetPath = $glyphRunTarget
+# $glyphStartMenuRunShortcutProcess.Save()
+
+# Create Start Menu uninstall shortcut
+Install-ChocolateyShortcut -ShortcutFilePath $glyphStartMenuUninstallShortcut -TargetPath $glyphUninstallTarget -WorkingDirectory "$glyphInstallDir\"
+# $glyphStartMenuUninstallShortcutWshShell = New-Object -comObject WScript.Shell
+# $glyphStartMenuUninstallShortcutProcess = $glyphStartMenuUninstallShortcutWshShell.CreateShortcut($glyphStartMenuUninstallShortcut)
+# $glyphStartMenuUninstallShortcutProcess.TargetPath = $glyphUninstallTarget
+# $glyphStartMenuUninstallShortcutProcess.Save()
+
+# Create Desktop shortcut
+Install-ChocolateyShortcut -ShortcutFilePath $glyphDesktopShortcut -TargetPath $glyphRunTarget -WorkingDirectory "$glyphInstallDir\"
+# $glyphDesktopShortcutWshShell = New-Object -comObject WScript.Shell
+# $glyphDesktopShortcutProcess = $glyphDesktopShortcutWshShell.CreateShortcut($glyphDesktopShortcut)
+# $glyphDesktopShortcutProcess.TargetPath = $glyphRunTarget
+# $glyphDesktopShortcutProcess.Save()
+
+# Create Protocol Registry entries
+New-Item "Registry::HKEY_CLASSES_ROOT\glyph" -Force
+New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\glyph" -Name "(Default)" -Value "URL:glyph protocol" -PropertyType "String" -Force
+New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\glyph" -Name "URL Protocol" -Value "" -PropertyType "String" -Force
+New-Item "Registry::HKEY_CLASSES_ROOT\glyph\Shell\Open\Command" -Force
+New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\glyph\Shell\Open\Command" -Name "(Default)" -Value "`"$glyphRunTarget`" -url `"%1`"" -PropertyType "String" -Force
+New-Item "HKCU:\Software\Classes\glyph" -Force
+New-ItemProperty -Path "HKCU:\Software\Classes\glyph" -Name "(Default)" -Value "URL:glyph protocol" -PropertyType "String" -Force
+New-ItemProperty -Path "HKCU:\Software\Classes\glyph" -Name "URL Protocol" -Value "" -PropertyType "String" -Force
+New-Item "HKCU:\Software\Classes\glyph\Shell\Open\Command" -Force
+New-ItemProperty -Path "HKCU:\Software\Classes\glyph\Shell\Open\Command" -Name "(Default)" -Value "`"$glyphRunTarget`" -url `"%1`"" -PropertyType "String" -Force
+
+# Create Run Registry entry
+New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "Glyph Client" -Value "C:\Program Files (x86)\Glyph\GlyphClient.exe -hidden" -PropertyType "String" -Force
+
+# Create Glyph App Registry entries
+New-Item "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "DisplayName" -Value "Glyph" -PropertyType "String" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "DisplayIcon" -Value "C:\Program Files (x86)\Glyph\GlyphClient.exe" -PropertyType "String" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "Publisher" -Value "Trion Worlds, Inc." -PropertyType "String" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "InstallDate" -Value "$(Get-Date -UFormat "%Y%m%d")" -PropertyType "String" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "InstallLocation" -Value "C:\Program Files (x86)\Glyph\" -PropertyType "String" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "InstallSource" -Value $glyphExtractUnzipFileDir -PropertyType "String" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "VersionMajor" -Value 1 -PropertyType "DWord" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "VersionMinor" -Value 0 -PropertyType "DWord" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "EstimatedSize" -Value "$((Get-ChildItem $glyphInstallDir -recurse | Measure-Object -property length -sum).Sum -replace ".{3}$")" -PropertyType "DWord" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "UninstallString" -Value "C:\Program Files (x86)\Glyph\glyphuninstall.exe" -PropertyType "String" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "HelpLink" -Value "http://glyph.trionworlds.com" -PropertyType "String" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "URLInfoAbout" -Value "http://glyph.trionworlds.com" -PropertyType "String" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "URLUpdateInfo" -Value "http://glyph.trionworlds.com" -PropertyType "String" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "Language" -Value 9 -PropertyType "DWord" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "NoModify" -Value 1 -PropertyType "DWord" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Name "NoRepair" -Value 1 -PropertyType "DWord" -Force
--- glyph-launcher/tools/chocolateyuninstall.ps1
+++ glyph-launcher/tools/chocolateyuninstall.ps1
@@ -1,30 +1,51 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$ahkExe     = 'AutoHotKey'
-$ahkFile    = "$toolsDir\glyph-launcher_uninstall.ahk"
+$glyphInstallDir = Join-Path ${env:ProgramFiles(x86)} "Glyph"
+$glyphProgramDataConfig = Join-Path $env:ProgramData "Glyph\GlyphLibrary.cfg"
+$glyphStartMenuRunShortcutFolder = Join-Path $env:ProgramData "Microsoft\Windows\Start Menu\Programs\Glyph"
+$glyphDesktopShortcut = Join-Path "$([Environment]::GetFolderPath("Desktop"))" "Glyph.lnk"
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'Glyph*'
-  fileType      = 'EXE'
-  validExitCodes= @(0, 3010, 1605, 1614, 1641, 1)
-}
+Start-WaitandStop "Glyph*"
 
-[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+Remove-Item $glyphInstallDir -Recurse -Force
+Remove-Item $glyphProgramDataConfig -Force
+Remove-Item $glyphStartMenuRunShortcutFolder -Recurse -Force
+Remove-Item $glyphDesktopShortcut -Force
+Remove-Item "Registry::HKEY_CLASSES_ROOT\glyph" -Recurse -Force
+# Remove-Item "HKCU:\Software\Classes\glyph" -Recurse -Force
+Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "Glyph Client" -Force
+Remove-Item "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Recurse -Force
 
-if ($key.Count -eq 1) {
-  $key | % {
-    $packageArgs['file'] = "$($_.UninstallString)"
+# $packageArgs = @{
+#   packageName   = $env:ChocolateyPackageName
+#   softwareName  = 'Glyph*'
+#   fileType      = 'EXE'
+#   silentArgs    = "-uninstall -silent"
+#   validExitCodes= @(0, 3010, 1605, 1614, 1641, 1)
+# }
 
-    Start-Process $ahkExe $ahkFile
-    Uninstall-ChocolateyPackage @packageArgs
-    Start-Sleep -s 12
-  }
-} elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
-}
+# [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+# if ($key.Count -eq 1) {
+#   $key | % {
+#     $packageArgs['file'] = "$($_.InstallLocation)GlyphClient.exe"
+
+#     Uninstall-ChocolateyPackage @packageArgs
+
+#     Remove-Item $glyphInstallDir -Recurse -Force
+#     Remove-Item $glyphProgramDataConfig -Force
+#     Remove-Item $glyphStartMenuRunShortcutFolder -Recurse -Force
+#     Remove-Item $glyphDesktopShortcut -Force
+#     Remove-Item "Registry::HKEY_CLASSES_ROOT\glyph" -Recurse -Force
+#     # Remove-Item "HKCU:\Software\Classes\glyph" -Recurse -Force
+#     Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "Glyph Client" -Force
+#     Remove-Item "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Glyph" -Recurse -Force
+#   }
+# } elseif ($key.Count -eq 0) {
+#   Write-Warning "$packageName has already been uninstalled by other means."
+# } elseif ($key.Count -gt 1) {
+#   Write-Warning "$($key.Count) matches found!"
+#   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+#   Write-Warning "Please alert package maintainer the following keys were matched:"
+#   $key | % {Write-Warning "- $($_.DisplayName)"}
+# }
