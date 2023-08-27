cf57a1fc4c286e02838b51c461a10a9ba05c030d
Zoull <zoull@zdindustries.com>
2023-08-26 21:19:11 -0600

    AU: 1 updated - rockstar-launcher [skip ci]

--- rockstar-launcher/tools/chocolateyinstall.ps1
+++ rockstar-launcher/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $ahkFile  = "$toolsDir\rockstar-launcher_install.ahk"
 $url      = 'https://gamedownloads.rockstargames.com/public/installer/Rockstar-Games-Launcher.exe'
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$checksum = '3a4f033cf88ee894f6b4d5c0b079b669eaf9a55a04c4a18f76ce26ec9464cb7c'
+$checksum = 'cd2fc9a051f406ae101d1844b4f764f6d70a06f01234dcd9044f745a0d297451'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
