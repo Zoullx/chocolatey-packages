7cc9f1f146da73ad2baebacf62024cba86b8178c
Zoull <zoull@zdindustries.com>
2023-10-23 15:49:41 -0600

    Uncomment uninstall script because auto uninstaller doesn't actually uninstall EVE Online

--- eve-online/tools/chocolateyuninstall.ps1
+++ eve-online/tools/chocolateyuninstall.ps1
@@ -1,29 +1,29 @@
-﻿# $ErrorActionPreference = 'Stop';
-# $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-# $packageArgs = @{
-#   packageName   = $env:ChocolateyPackageName
-#   softwareName  = 'eve-online'
-#   fileType      = 'EXE'
-#   silentArgs    = '--uninstall -s'
-#   validExitCodes= @(0, 3010, 1605, 1614, 1641)
-# }
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'eve-online'
+  fileType      = 'EXE'
+  silentArgs    = '--uninstall -s'
+  validExitCodes= @(0, 3010, 1605, 1614, 1641)
+}
 
-# [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
-# if ($key.Count -eq 1) {
-#   $key | % {
-#     $file = $_.UninstallString -Split " -" | Select-Object -First 1
+if ($key.Count -eq 1) {
+  $key | % {
+    $file = $_.UninstallString -Split " -" | Select-Object -First 1
 
-#     $packageArgs['file'] = $file
+    $packageArgs['file'] = $file
 
-#     Uninstall-ChocolateyPackage @packageArgs
-#   }
-# } elseif ($key.Count -eq 0) {
-#   Write-Warning "$packageName has already been uninstalled by other means."
-# } elseif ($key.Count -gt 1) {
-#   Write-Warning "$($key.Count) matches found!"
-#   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-#   Write-Warning "Please alert package maintainer the following keys were matched:"
-#   $key | % {Write-Warning "- $($_.DisplayName)"}
-# }
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
