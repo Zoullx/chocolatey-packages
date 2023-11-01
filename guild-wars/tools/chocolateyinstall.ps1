1309b2e6bbe40de407eae728ad4fa82683b5b8fe
Zoull <zoull@zdindustries.com>
2023-10-31 21:17:25 -0600

    Update install script to check if Guild Wars is already installed

--- guild-wars/tools/chocolateyinstall.ps1
+++ guild-wars/tools/chocolateyinstall.ps1
@@ -4,16 +4,21 @@ $ahkExe = 'AutoHotKey'
 $ahkFile = "$toolsDir\guild-wars_install.ahk"
 $url = 'https://cloudfront.guildwars2.com/client/GwSetup.exe'
 
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  unzipLocation  = $toolsDir
-  fileType       = 'EXE'
-  url            = $url
-  softwareName   = 'Guild Wars'
-  checksum       = '9152740C834EEF57DB881B51B46149E0BEDEBC65DEEF0900050ABD0C41CE1C4B'
-  checksumType   = 'sha256'
-  validExitCodes = @(0, 3010, 1641)
+if (-not (Test-Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Guild Wars")) {
+  $packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'EXE'
+    url            = $url
+    softwareName   = 'Guild Wars'
+    checksum       = '9152740C834EEF57DB881B51B46149E0BEDEBC65DEEF0900050ABD0C41CE1C4B'
+    checksumType   = 'sha256'
+    validExitCodes = @(0, 3010, 1641)
+  }
+  
+  Start-Process $ahkExe $ahkFile
+  Install-ChocolateyPackage @packageArgs
 }
-
-Start-Process $ahkExe $ahkFile
-Install-ChocolateyPackage @packageArgs
+else {
+  Write-Host "Guild Wars is already installed"
+}
