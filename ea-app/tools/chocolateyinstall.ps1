9d7cafd96a498db0838b6295e4d494a1d32b6e5a
Zoull <zoull@zdindustries.com>
2024-02-16 17:54:50 -0700

    Update EA App to version 13.128.0.5641

--- ea-app/tools/chocolateyinstall.ps1
+++ ea-app/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url      = 'https://origin-a.akamaihd.net/EA-Desktop-Client-Download/installer-releases/EAappInstaller.exe'
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$checksum = 'c34ef956264e13f4ed0d2d8f948a03eac98b4a0971240b19f6f855191e060507'
+$checksum = 'f76d816e4b1560613780011e4418d17a957ce720a29dd0e1bb1bfa7c47b28e60'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
