24792ee0e9ea507eb46f14fcd5f9962937b9bf02
Zoull <zoull@zdindustries.com>
2024-02-21 11:50:37 -0700

    Update League of Legends to version 80.0.0.1034

--- leagueoflegends/tools/chocolateyinstall.ps1
+++ leagueoflegends/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe'
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$checksum = 'e9d9146a6c195e59dd37fa2fd6a96be3b4f3f02731ec6bba39d9578de261421a'
+$checksum = 'e5b332fb8b84eca20c4a94b84736233dc3fdd6d5d6b09cf7c75b982f35dc7751'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
