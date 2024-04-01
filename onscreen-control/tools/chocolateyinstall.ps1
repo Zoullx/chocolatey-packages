d1ceaa4df9a008b091b68736b557054a379f111f
Zoull <zoull@zdindustries.com>
2024-03-31 18:29:12 -0600

    Update OnScreen Control to version 8.26.0.0

--- onscreen-control/tools/chocolateyinstall.ps1
+++ onscreen-control/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $checksumType = 'sha256'
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$url      = 'https://gscs-b2c.lge.com/downloadFile?fileId=nUPbP289G9DRT8bqAsiCA'
-$checksum = '73c976e780892f56285b0b1de79142157dce1bacf0e99fa0657411bd0489bca6'
+$url = 'https://lmu.lge.com/ExternalService/onscreencontrol/win/2.0/OnScreenControl_8.26.zip'
+$checksum = '51096DDF31E528C36E59CDC06472A7CFAF9B3A0D348855EF1424C6F91FBCFF67'
 
 $zipPackageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -25,21 +25,23 @@ if ($installed) {
   $installedMinor = (Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{E5C1B339-0E4E-49A5-859E-5E1DE1938706}" -Name VersionMinor).VersionMinor
   if ("$packageMajor.$packageMinor" -ne "$installedMajor.$installedMinor") {
     $silentScript = Join-Path $toolsDir 'upgrade.iss'
-  } else {
+  }
+  else {
     $silentScript = Join-Path $toolsDir 'reinstall.iss'
   }
-} else {
+}
+else {
   $silentScript = Join-Path $toolsDir 'install.iss'
 }
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  file          = $fileLocation
-  softwareName  = 'OnScreen Control'
-  silentArgs    = "-f1`"$silentScript`" -s"
-  validExitCodes= @(0, 3010, 1641)
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  file           = $fileLocation
+  softwareName   = 'OnScreen Control'
+  silentArgs     = "-f1`"$silentScript`" -s"
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
