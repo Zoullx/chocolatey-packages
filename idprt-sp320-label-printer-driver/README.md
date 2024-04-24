c284db17cdd96d81d2fc3a548f5228bbbc2f9714
Zoull <zoull@zdindustries.com>
2024-04-24 15:04:24 -0600

    Add iDPRT TSPL SP320 Thermal Label Printer Windows Driver Chocolatey package

---
+++ idprt-sp320-label-printer-driver/README.md
@@ -0,0 +1,5 @@
+﻿![idprt-sp320-label-printer-driver Logo](https://raw.githubusercontent.com/Zoullx/chocolatey-packages/master/idprt-sp320-label-printer-driver/idprt-sp320-label-printer-driver.png "iDPRT TSPL SP320 Label Printer Windows Driver Logo")
+
+# iDPRT TSPL SP320 Thermal Label Printer Windows Driver
+
+Windows driver for the iDPRT SP320 Thermal Label Printer
---
+++ idprt-sp320-label-printer-driver/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://www.idprt.com/prt_v2/files/down_file/id/275/fid/471.html'
+
+$zipPackageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  url           = $url
+  unzipLocation = $toolsDir
+  checksum      = 'F25B4BC9A705ACEECF3FA7865C23A3535A1D5BB418696DA4042646A75C2084AC'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @zipPackageArgs
+
+$fileName = (Get-ChildItem $toolsDir -Filter *.exe | Select-Object -First 1).Name
+$fileLocation = Join-Path $toolsDir $fileName
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  file           = $fileLocation
+  softwareName   = 'iDPRT TSPL Label Printer Windows Driver*'
+  checksum       = '96446DB5007A555599F37210BB3BE0C03AE624A3B5175F996ACC1C113A99A7CA'
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
