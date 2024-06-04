bc164aa3fab5cea06170dbb814db322834a9d8dc
Zoull <zoull@zdindustries.com>
2024-06-04 17:14:27 -0600

    Add Steam

---
+++ steam/README.md
@@ -0,0 +1,5 @@
+﻿![steam Logo](https://raw.githubusercontent.com/Zoullx/chocolatey-packages/master/steam/steam.png "Steam Logo")
+
+# Steam
+
+Steam is a digital distribution platform developed by Valve Corporation for purchasing and playing video games. Steam offers digital rights management, matchmaking servers, video streaming, and social networking services.
---
+++ steam/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://media.steampowered.com/client/installer/SteamSetup.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  softwareName   = 'Steam'
+  checksum       = '7D3654531C32D941B8CAE81C4137FC542172BFA9635F169CB392F245A0A12BCB'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
