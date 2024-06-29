a6f60a29cda8198b719d7996f688816d331ccc75
Zoull <zoull@zdindustries.com>
2024-06-29 14:46:48 -0600

    Update League of Legends to version 88.0.1.1613

--- leagueoflegends/tools/chocolateyinstall.ps1
+++ leagueoflegends/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe'
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$checksum = 'c90e6fd0c61486eaecb8b95e60bcf5a4a1f90e526ad6b7b4715a939740568845'
+$checksum = '8e1243d37ec8e3b2e49bb1f52e832830194931453551b1ccaf33ee8b9d0f6e4a'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
