908b85e3fc9bab5821b66afa642ae1495ac1d46e
Zoull <zoull@zdindustries.com>
2024-11-14 16:52:45 -0700

    Created new EVE Online Launcher package and deprecated old package so there wouldn't be version weirdness

---
+++ eve-online-new/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://launcher.ccpgames.com/eve-online/release/win32/x64/eve-online-latest+Setup.exe'
+
+# DO NOT CHANGE THESE MANUALLY, USE update.ps1
+$checksum = 'fbdf905c2d07cf2afd090cddadd15e652cb8020b4fd298f616fa8050070b67de'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  softwareName   = 'eve-online'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = '/silent'
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
