7c34ab73175fcdb864af6d1a411f39c33120bf7f
Zoull <zoull@zdindustries.com>
2024-11-19 10:43:39 -0700

    Add auto update script

---
+++ nvidia-app/update.ps1
@@ -0,0 +1,41 @@
+#Requires -Version 5.0
+#Requires -Modules chocolatey-au
+[cmdletbinding()]
+param (
+  [switch]$Force
+)
+
+$ErrorActionPreference = 'Stop'
+
+$downloadUrl = 'https://www.nvidia.com/en-us/software/nvidia-app/'
+
+function global:au_BeforeUpdate {
+  $Latest.Checksum32 = Get-RemoteChecksum -Url $Latest.Url32 -Algorithm 'SHA256'
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^[$]url\s*=\s*)'.*'"      = "`${1}'$($Latest.URL32)'"
+      "(?i)(^[$]checksum\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest $downloadUrl -UseBasicParsing
+  $url = $download_page.links.href -match 'NVIDIA_app.*\.exe$' | Select-Object -First 1
+  $url -match '.*_v(?<Version>.*)\.exe$'
+  $version = $matches.Version
+
+  return @{
+    Version = $version
+    URL32   = $url
+  }
+}
+
+# run the update only if this script is not sourced by the virtual package
+if ($MyInvocation.InvocationName -ne '.') {
+  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+  Update-Package -NoCheckUrl -NoCheckChocoVersion -NoReadme -ChecksumFor none -Force:$Force
+}
