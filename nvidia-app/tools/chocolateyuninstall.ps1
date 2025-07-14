c36ae5d6b8c7e7c3208722543c9bb769b244a42c
Zoull <zoull@zdindustries.com>
2025-07-14 15:06:38 -0600

    Update NVIDIA App to version 11.0.4.159

--- nvidia-app/tools/chocolateyuninstall.ps1
+++ nvidia-app/tools/chocolateyuninstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://us.download.nvidia.com/nvapp/client/11.0.3.218/NVIDIA_app_v11.0.3.218.exe'
+$url = 'https://us.download.nvidia.com/nvapp/client/11.0.4.159/NVIDIA_app_v11.0.4.159.exe'
 $fileName = [System.IO.Path]::GetFileName($url)
 $fileLocation = Join-Path $toolsDir $fileName
-$checksum = 'c19a150e53427175e5996100a25ed016f6730b627b1d6b85813811a8751c77b7'
+$checksum = 'd3bb98e7512863d7e74621efdd9ecd223a699543dc22be7e4f791bb81db3b6c3'
 
 $webFileArgs = @{
   packageName  = $env:ChocolateyPackageName
