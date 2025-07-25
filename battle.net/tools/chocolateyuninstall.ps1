14e009b5aebc2eca09734567ac62596b6d8dd95e
Zoull <zoull@zdindustries.com>
2025-07-25 13:07:25 -0600

    Update path variables to use proper environment variables

--- battle.net/tools/chocolateyuninstall.ps1
+++ battle.net/tools/chocolateyuninstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 Stop-Process -Name Agent -Force -ErrorAction SilentlyContinue
 Stop-Process -Name Battle.net -Force -ErrorAction SilentlyContinue
-Remove-Item "%LocalAppData%\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item "%LocalAppData%\Blizzard Entertainment" -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item "%AppData%\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item "%ProgramData%\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item "%ProgramFiles%\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item "%ProgramFiles(x86)%\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item "%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item "%PUBLIC%\Desktop\Battle.net.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item "${env:LocalAppData}\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item "${env:LocalAppData}\Blizzard Entertainment" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item "${env:AppData}\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item "${env:ProgramData}\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item "${env:ProgramFiles}\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item "${env:ProgramFiles(x86)}\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item "${env:ALLUSERSPROFILE}\Microsoft\Windows\Start Menu\Programs\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item "${env:PUBLIC}\Desktop\Battle.net.lnk" -Force -ErrorAction SilentlyContinue
 Remove-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Battle.net" -Recurse -Force -ErrorAction SilentlyContinue
