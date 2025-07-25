60da83891b3fd3a8397ba4b6acbcfaf7fac27793
Zoull <zoull@zdindustries.com>
2025-07-25 13:06:47 -0600

    Add HereLabel package

---
+++ here-label/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url = 'https://overseas.hprtcloud.com/upgrade/app_version/2025_07_21/1WG2LwxopvqxDgoS/HereLabel%20V1.04.05.exe'
+$checksum = '060C842B51CDA6ABCF596E1A0A2C185E9192C05D969C4874FD14240AFC04F6F3'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  softwareName   = 'HereLabel*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
