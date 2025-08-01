c8126ad661011b67885309280e36433d0a6fa93f
Zoull <zoull@zdindustries.com>
2025-08-01 09:29:30 -0600

    Fix uninstalls

--- razer-synapse-4/tools/chocolateybeforemodify.ps1
+++ razer-synapse-4/tools/chocolateybeforemodify.ps1
@@ -7,9 +7,32 @@
 # NOTE: For upgrades - like the uninstall script, this script always runs from
 #  the currently installed version, not from the new upgraded package version.
 $ErrorActionPreference = 'Stop';
+$razerStringTranslationsPackageArgs = @{
+  packageName    = 'Razer String Translations'
+  file           = 'C:\ProgramData\Razer\Synapse3\Uninstall\RazerStringTranslations\RazerStringTranslationsUninstall.exe'
+  fileType       = 'EXE'
+  silentArgs     = '/S'
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
+
+$razerSynapseConfigurationDataPackageArgs = @{
+  packageName    = 'Razer Synapse Configuration Data'
+  file           = 'C:\ProgramData\Razer\Synapse3\Uninstall\RazerSynapseConfigurationData\RazerSynapseConfigurationDataUninstall.exe'
+  fileType       = 'EXE'
+  silentArgs     = '/S'
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
+
+$razerSynapsePackageArgs = @{
+  packageName    = 'Razer Synapse'
+  file           = 'C:\ProgramData\Razer\Synapse3\Uninstall\RazerSynapse\RazerSynapseUninstall.exe'
+  fileType       = 'EXE'
+  silentArgs     = '/S'
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
 
 if (Test-Path -Path "HKLM:\SOFTWARE\Razer\Synapse3") {
-  & "C:\ProgramData\Razer\Synapse3\Uninstall\RazerStringTranslations\RazerStringTranslationsUninstall.exe" "/S"
-  & "C:\ProgramData\Razer\Synapse3\Uninstall\RazerSynapseConfigurationData\RazerSynapseConfigurationDataUninstall.exe" "/S"
-  & "C:\ProgramData\Razer\Synapse3\Uninstall\RazerSynapse\RazerSynapseUninstall.exe" "/S"
+  Uninstall-ChocolateyPackage @razerStringTranslationsPackageArgs
+  Uninstall-ChocolateyPackage @razerSynapseConfigurationDataPackageArgs
+  Uninstall-ChocolateyPackage @razerSynapsePackageArgs
 }
--- razer-synapse-4/tools/chocolateyuninstall.ps1
+++ razer-synapse-4/tools/chocolateyuninstall.ps1
@@ -1,7 +1,46 @@
 ﻿$ErrorActionPreference = 'Stop'
+$razerAppEnginePackageArgs = @{
+  packageName    = 'Razer App Engine'
+  file           = 'C:\Windows\Installer\Razer\RazerAppEngine\uninstall.exe'
+  fileType       = 'EXE'
+  silentArgs     = '"/U:C:\Windows\Installer\Razer\RazerAppEngine\uninstall.xml" /S'
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
 
-& "C:\Windows\Installer\Razer\RazerAppEngine\uninstall.exe" """/U:C:\Windows\Installer\Razer\RazerAppEngine\uninstall.xml""" "/S"
-& "C:\Windows\Installer\Razer\Razer Synapse\uninstall.exe" """/U:C:\Windows\Installer\Razer\Razer Synapse\uninstall.xml""" "/S"
-& "C:\Windows\Installer\Razer\Razer Chroma\uninstall.exe" """/U:C:\Windows\Installer\Razer\Razer Chroma\uninstall.xml""" "/S"
-& "C:\Windows\Installer\Razer Central\RCUninstall.exe" """/U:C:\Windows\Installer\Razer Central\RCUninstall\uninstall.xml""" "/S"
-& "C:\Program Files (x86)\Razer\Razer Services\GMS3\unins000.exe" "/SP-" "/VERYSILENT" "/SUPRESSMSGBOXES" "/NORESTART"
+$razerSynapsePackageArgs = @{
+  packageName    = 'Razer Synapse 4'
+  file           = 'C:\Windows\Installer\Razer\Razer Synapse\uninstall.exe'
+  fileType       = 'EXE'
+  silentArgs     = '"/U:C:\Windows\Installer\Razer\Razer Synapse\uninstall.xml" /S'
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
+
+$razerGameManagerPackageArgs = @{
+  packageName    = 'Razer Game Manager'
+  file           = 'C:\Program Files (x86)\Razer\Razer Services\GMS3\unins000.exe'
+  fileType       = 'EXE'
+  silentArgs     = '/SP- /VERYSILENT /SUPRESSMSGBOXES /NORESTART'
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
+
+$razerChromaPackageArgs = @{
+  packageName    = 'Razer Chroma'
+  file           = 'C:\Windows\Installer\Razer\Razer Chroma\uninstall.exe'
+  fileType       = 'EXE'
+  silentArgs     = '"/U:C:\Windows\Installer\Razer\Razer Chroma\uninstall.xml" /S'
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
+
+$razerCentralPackageArgs = @{
+  packageName    = 'Razer Central'
+  file           = 'C:\Windows\Installer\Razer Central\RCUninstall.exe'
+  fileType       = 'EXE'
+  silentArgs     = '"/U:C:\Windows\Installer\Razer Central\RCUninstall\uninstall.xml" /S'
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
+
+Uninstall-ChocolateyPackage @razerAppEnginePackageArgs
+Uninstall-ChocolateyPackage @razerSynapsePackageArgs
+Uninstall-ChocolateyPackage @razerGameManagerPackageArgs
+Uninstall-ChocolateyPackage @razerChromaPackageArgs
+Uninstall-ChocolateyPackage @razerCentralPackageArgs
