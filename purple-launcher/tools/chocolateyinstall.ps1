7825191178a442a4ea1039f574dbdac79dd4e958
Zoull <zoull@zdindustries.com>
2025-08-11 12:21:39 -0600

    Update Purple Launcher to version 2.25.728.14 and fix install

--- purple-launcher/tools/chocolateyinstall.ps1
+++ purple-launcher/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url = 'https://dw.uptodown.net/dwn/RvVkii134Riphftvun7hQBZyU0aCwJjJMFI3FD3XyiRG_EkDcqCx3GdZA6pJEYTIXrvo6lqoOlYyP3U76hSXO3MOFvm-fbXPfmMIX0hgTEl1LZ7Elt1VdOIl7Ozcb40g/wUUUrrKKn47QfLZ1txkler3aGHxNree99keo59iJc0N2OhqWTXk261Wm1Km2GtsiwA2dc9M5RcLOFMc122KaAsqNqXw9XDKrLwKVy67sDrvt09NfWn5sYNyuiQBNYLco/G2KGhx9NUfJtkxNUom1w2A-_XHFMepYxgkJ5RfyStTp2ZAv46H2JwX6rzQsj1CsI_6fIRHW1bFd1k_HmrK0bag==/purple-2-25-716-22.exe'
-$checksum = 'FC82388A209C53DEB638E498B95A7435058FF1F4327D9545B442C818AAA57CBC'
+$url = 'https://dw.uptodown.net/dwn/g081nXyZ9qx5JnstDDnoCR3IcZv5cxyuqdt4FEK7kdJa0PvM61jLA72PcCLFLwRXvzKyzTBsNuKsxJ8XnITyMygop-nFRFMqVN2veZsKaEEcGvukA2ivRH7H7GAHwUQy/7zdyYeoKoq6BLJN2nnLsGE1apIKALOsmWZ-e5ghtZZ487TrHk_ANY6L6EDK9SjgP_suXt-70YuJc9AYjlSql8ipVI5T6UX7kTASNG7W-QtSRgLGVdJ8t8wsR4mN4Tjwb/MSISFGWUq-Cikl0CROgZIiZ2XmSpx0IhE5_W6jsB7TxonjIuCek_rToSS7MkewwgvLE-AKXrsAaFizjXJarTEA==/purple-2-25-728-14.exe'
+$checksum = 'C992513DAAE37306975591A2BB73C1C383B2BBDD452EDB9FEDEFFF4CF1CB01E5'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
