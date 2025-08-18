a2cbb321e4b524d70b9d991f6ce4d067be1eb321
Zoull <zoull@zdindustries.com>
2025-08-18 11:42:58 -0600

    Update EVE Online (New) to version 1.11.1

--- eve-online-new/tools/chocolateyinstall.ps1
+++ eve-online-new/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://launcher.ccpgames.com/eve-online/release/win32/x64/eve-online-latest+Setup.exe'
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$checksum = '622dbbe888bf2e954512f9f7604d46af94f044ed4614f57277965d274c8a9f9a'
+$checksum = '17abcb55fc61d654839e45fb6ed00809e1c66e9d3bb84e473b0d9ea5dca1ee3d'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
