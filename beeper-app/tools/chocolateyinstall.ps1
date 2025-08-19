76b445dfad1ef6651e34713ed9cbd80903d212d4
Zoull <zoull@zdindustries.com>
2025-08-18 18:14:25 -0600

    Update download link for Beeper App

--- beeper-app/tools/chocolateyinstall.ps1
+++ beeper-app/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://dw.uptodown.com/dwn/RvVkii134Riphftvun7hQBZyU0aCwJjJMFI3FD3XyiSzvjH_FvzdGUAmcDD622WCObvj44nyAyR7Jm2iOS9lupNAcMFDJhll-J3dTfrEH14wHLCtXmP6v27x8px7Johr/O01pbOMfh3GBYEX9O5b9ExJtmHhZ00RIH4-Cbf07HFHDk2XZDa4lgjdq3zHi1zfD174G5ULyf8bGE0dtrMDFCNFYGDXCrRpZDf_5n4ukQDhCrDs9f4cg96WtXQBKo7Nr/W7EWTqA3mlWEYCGj18TVcUoLt1ZK5kFWd8Kq7B15GyyRdSQR4_GNZGg5O7Ot8ST_0r61mMF9i5Z_91KQRSu27g==/'
+$url = 'https://beeper-desktop.download.beeper.com/builds/Beeper%20x64%204.1.111.exe'
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
 $checksum = '9efb222fbf60fc1148d3d9c23cc861c7e98c17321868e1735445248dd2ff20c8'
