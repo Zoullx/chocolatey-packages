c756eb0e703684733e1b7caeefa4fb54000bf617
Zoull <zoull@zdindustries.com>
2025-10-16 11:25:07 -0600

    Update Beeper to version 4.1.289

--- beeper-app/tools/chocolateyinstall.ps1
+++ beeper-app/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$url = 'https://beeper-desktop.download.beeper.com/builds/Beeper%20x64%204.1.265.exe'
-$checksum = 'cb3d82c87563e6b69f7a3ba133abc8f4e79240f3981d79d4bb6ddd97bfb42360'
+$url = 'https://beeper-desktop.download.beeper.com/builds/Beeper%20x64%204.1.289.exe'
+$checksum = '1466fdb88e97fec3cd62e7bd35951b82547c5270dfd6f0d216e798617ff3c192'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
