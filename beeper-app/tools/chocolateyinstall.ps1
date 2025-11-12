6fc79c08d864a249134990a585c27d60e97a1cdc
Zoull <zoull@zdindustries.com>
2025-11-12 11:38:15 -0700

    AU: 1 updated - beeper-app [skip ci]

--- beeper-app/tools/chocolateyinstall.ps1
+++ beeper-app/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$url = 'https://beeper-desktop.download.beeper.com/builds/Beeper%20x64%204.2.166.exe'
-$checksum = '8bd9f9110ac44f48a76ffa724914f334c0e5d0be8e9cca87df4c95afb5fffd6e'
+$url = 'https://beeper-desktop.download.beeper.com/builds/Beeper%20x64%204.2.179.exe'
+$checksum = '83ff0a651b1e2bd0f010129564a3b57c581646ba0c467ec6dcb91071eae5508b'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
