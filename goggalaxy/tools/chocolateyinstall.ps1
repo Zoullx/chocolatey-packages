6ed18ca69222f9e813c84a77a39578db5dd0fc42
Zoull <zoull@zdindustries.com>
2026-02-08 08:33:56 -0700

    Update GOG Galaxy to version 2.0.93.55

--- goggalaxy/tools/chocolateyinstall.ps1
+++ goggalaxy/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
-  url            = 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/setup_galaxy_2.0.92.24.exe'
+  url            = 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/setup_galaxy_2.0.93.55.exe'
   softwareName   = 'GOG Galaxy*'
-  checksum       = '9c8e3e25c8b01cce91e1204ae01e77bd'
+  checksum       = '7d926b84436c0a497d327be430113c7d'
   checksumType   = 'md5'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0, 1) # Code 1 - Same or newer version already installed. Uninstall before trying to downgrade.
