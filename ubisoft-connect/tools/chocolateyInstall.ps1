df5bfc2bee3dba7e7971a4dfa8e098a15aec5c22
Zoull <zoull@zdindustries.com>
2026-02-13 12:58:13 -0700

    Update Ubisoft Connect to version 169.6.0.13045

--- ubisoft-connect/tools/chocolateyInstall.ps1
+++ ubisoft-connect/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$checksum = 'e3c55d0f47bd90d6f5095c1ded2c7d6f314a6d1262b7e37c144b4da2f31115e33e0e5bcc389a5cf9d5b67726a58933d713d4418761fbe61872ae97c31baf98c0'
+$checksum = '982e5f149aec4df8927057e225f87871c7382e964fd21700fb9905c94f1832ac666ba5284b834ec0386b1fb7933800281f633d77fa0bc842b8f5a210175727e5'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
