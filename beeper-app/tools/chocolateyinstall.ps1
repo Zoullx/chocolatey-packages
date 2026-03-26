e4eb95eb2bc034bc4fd88bf73b58b278c37502c7
Zoull <zoull@zdindustries.com>
2026-03-26 09:13:01 -0600

    AU: 2 updated - beeper-app eve-online-new [skip ci]

--- beeper-app/tools/chocolateyinstall.ps1
+++ beeper-app/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$url = 'https://beeper-desktop.download.beeper.com/builds/Beeper%20x64%204.2.653.exe'
-$checksum = 'd223c5028c0586ddc4b631a23f3980e334899b5ecbea606b187da8716116afa6'
+$url = 'https://beeper-desktop.download.beeper.com/builds/Beeper%20x64%204.2.670.exe'
+$checksum = '6f4aeb88b489ea87d87a1f5a89e44500705f616a3cd5151f1447236a690a3a59'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
--- eve-online-new/tools/chocolateyinstall.ps1
+++ eve-online-new/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://launcher.ccpgames.com/eve-online/release/win32/x64/eve-online-latest+Setup.exe'
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$checksum = 'e7863d0b1e0d97a6cd3089a98d0dbc0c5d3ef5db661a9b363dadf49c18c01b0a'
+$checksum = '2a7342492f847d4e12efb94ad0d14eae2107b776f47d6271ec41f81fdfbe7a10'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
