8545436507a59fcfc597071069346d2ae8568116
Zoull <zoull@zdindustries.com>
2026-04-09 08:36:59 -0600

    AU: 1 updated - nvidia-app [skip ci]

--- nvidia-app/tools/chocolateyinstall.ps1
+++ nvidia-app/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://us.download.nvidia.com/nvapp/client/11.0.6.383/NVIDIA_app_v11.0.6.383.exe'
-$checksum = 'ce4cb43a778b177973dbeea9c3ffef5cfd6d04769cf106bc07ee11257e99e098'
+$url = 'https://us.download.nvidia.com/nvapp/client/11.0.7.237/NVIDIA_app_v11.0.7.237.exe'
+$checksum = 'bb0dcc80e30a4d807e6e87c1f879146a117ce57cebe1f79cfd8de7dd1563b111'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
--- nvidia-app/tools/chocolateyuninstall.ps1
+++ nvidia-app/tools/chocolateyuninstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://us.download.nvidia.com/nvapp/client/11.0.6.383/NVIDIA_app_v11.0.6.383.exe'
+$url = 'https://us.download.nvidia.com/nvapp/client/11.0.7.237/NVIDIA_app_v11.0.7.237.exe'
 $fileName = [System.IO.Path]::GetFileName($url)
 $fileLocation = Join-Path $toolsDir $fileName
-$checksum = 'ce4cb43a778b177973dbeea9c3ffef5cfd6d04769cf106bc07ee11257e99e098'
+$checksum = 'bb0dcc80e30a4d807e6e87c1f879146a117ce57cebe1f79cfd8de7dd1563b111'
 
 $webFileArgs = @{
   packageName  = $env:ChocolateyPackageName
