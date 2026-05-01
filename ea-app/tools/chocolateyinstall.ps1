b8716860de403810feedfb62e6d6da7f031ace9e
Zoull <zoull@zdindustries.com>
2026-05-01 08:27:34 -0600

    AU: 1 updated - ea-app [skip ci]

--- ea-app/tools/chocolateyinstall.ps1
+++ ea-app/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://origin-a.akamaihd.net/EA-Desktop-Client-Download/installer-releases/EAappInstaller.exe'
 
 # DO NOT CHANGE THESE MANUALLY, USE update.ps1
-$checksum = '01eed4ce5f357ead171b5d0f35baad2a17994d0d1b04f0ba7a62e0fe9b2ae03a'
+$checksum = '870636652df3b579feb64d7919525713d473236b36aeed01a8415504437538ed'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
